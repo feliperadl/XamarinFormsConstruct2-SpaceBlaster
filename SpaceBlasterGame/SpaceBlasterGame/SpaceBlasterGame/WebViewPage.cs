b611c7c6de04d3a59e30e6913fe1fe51be94d637
mumu <felipemumuradl@hotmail.com>
2017-06-18 16:59:59 -0300

    Project created

---
+++ SpaceBlasterGame/SpaceBlasterGame/SpaceBlasterGame/WebViewPage.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using Xamarin.Forms;
+
+namespace SpaceBlasterGame
+{
+    class WebViewPage : ContentPage
+    {
+        public WebViewPage()
+        {
+            Label header = new Label
+            {
+                Text = "Xamarin & Construct2",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+            var source = new HtmlWebViewSource();
+            var assetManager = Xamarin.Forms.Forms.Context.Assets;
+            using (var streamReader = new StreamReader(assetManager.Open("index.html")))
+            {
+                source.Html = streamReader.ReadToEnd();
+            }
+            
+            WebView webView = new WebView
+            {
+                Source = source,
+                VerticalOptions = LayoutOptions.FillAndExpand
+            };
+            
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    webView
+                }
+            };
+        }
+    }
+
+}
